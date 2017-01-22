d0bd714f4a1bf1094f13ee0954e5e7e5478ac139
Zyryanov Aleksander <zyryanov.aleksander@outlook.com>
2017-01-22 18:02:03 +1000

    Task 1.8.5

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf
+sudo rm /etc/nginx/sites-enabled/default
+sudo ﻿ln -sf /home/box/web/etc/nginx.conf
 sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf
 #sudo /etc/init.d/gunicorn restart
